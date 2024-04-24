0275526315afb39960b410d2943aa14c7e41a625
Sunny <Sunnylaw3571@gmail.com>
2024-04-24 11:33:49 +0100

    start driving

--- src/main/java/vehicles/Car.java
+++ src/main/java/vehicles/Car.java
@@ -3,6 +3,20 @@ package vehicles;
 public class Car extends Vehicle {
     public Car(String make, String model) {
         super(make, model);
-        this.engine = CarEngine;
+        this.engine = new CarEngine();
+    }
+
+    @Override
+    protected void accelerate() {
+        String message = make + " " + model + " speed: ";
+        double fuelMod = switch (this.engine.fuelType) {
+            case "Unleaded" -> 1.5D;
+            case "Leaded" -> 1.2D;
+            case "Diesel" -> 1.8D;
+            default -> throw new IllegalStateException("Unexpected value: " + this.engine.fuelType);
+        };
+        for (int i = 0 ; i < 10 ; i++) {
+            System.out.println(message + String.format("%.2f", (engine.horsepower * fuelMod) * Math.exp(Math.sqrt(i)) / 2));
+        }
     }
 }
---
+++ src/main/java/vehicles/Main.java
@@ -0,0 +1,14 @@
+package vehicles;
+
+public class Main {
+    public static void main(String[] args) {
+        Car toyota = new Car("2010", "Micra");
+        Motorcycle tesla = new Motorcycle("2010", "model e", true);
+
+//        toyota.start();
+//        toyota.drive();
+//        tesla.start();
+//        tesla.accelerate();
+//        tesla.drive();
+    }
+}
